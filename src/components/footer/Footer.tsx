59c21710fa015d4cd8638c45791122bc36694886
tarchevsky <i.tarchevsky@gmai.com>
2024-11-14 19:10:58 +0300

    Ширина картинки full в footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Footer = () => {
 	return (
 		<footer className='footer'>
-			<Image src='/me-3.png' alt='' width={1920} height={1000} className='h-min-[400px]' />
+			<Image src='/me-3.png' alt='' width={1920} height={1000} className='h-min-[400px] w-full' />
 		</footer>
 	)
 }
