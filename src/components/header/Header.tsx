f20d506ee7bfd7bd415ff2c353a7df27496a56a5
tarchevsky <i.tarchevsky@gmai.com>
2024-11-14 17:27:33 +0300

    Переделал logo в header в заголовок

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,12 +3,13 @@ import cn from 'clsx'
 import styles from './Header.module.scss'
 
 import Link from 'next/link'
+import Htag from "@/components/Htag/Htag";
 
 const Header = () => {
 
 	return (
 		<header className='cont flex flex-col gap-1 pb-4'>
-			<Link href='/' className={cn(styles.logo)}>MARIE RAKHMANI</Link>
+			<Htag tag='h1' className={cn(styles.logo, 'uppercase')}>Marie Rakhmani</Htag>
 			<div className={cn(styles.subtitle)}>Бижутерия ручной работы из поделочного камня</div>
 		</header>
 	)
