9a7631ed881676bb4702efb35aea75b38d815197
tarchevsky <i.tarchevsky@gmai.com>
2024-11-17 15:22:20 +0300

    Добавили в наличии все позиции в cardsThree

--- src/data/cardsThree.ts
+++ src/data/cardsThree.ts
@@ -3,24 +3,24 @@ export const cardsThree = [
         src: '/cards/12.png',
         alt: '',
         name: 'Сказание о пылающем фениксе',
-        status: 'Нет в наличии',
+        status: 'Есть в наличии',
     },
     {
         src: '/cards/13.png',
         alt: '',
         name: 'Ближе к сердцу',
-        status: 'Нет в наличии',
+        status: 'Есть в наличии',
     },
     {
         src: '/cards/14.png',
         alt: '',
         name: 'Дуновение ветра в открытое окно',
-        status: 'Нет в наличии',
+        status: 'Есть в наличии',
     },
     {
         src: '/cards/15.png',
         alt: '',
         name: 'Дельфины',
-        status: 'Нет в наличии',
+        status: 'Есть в наличии',
     }
 ]
